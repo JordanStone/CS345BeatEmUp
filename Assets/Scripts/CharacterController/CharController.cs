f65214962eb71cfa9f09d3e194c8b20aa23dbddf
Jordan Stone <professoroflaserology@gmail.com>
2014-08-02 13:44:00 -0400

    Likely Last Commit for Today. Added More to Controller, Mainly Fill-Ins for Where Punching is Going to Happen.

--- Assets/Scripts/CharacterController/CharController.cs
+++ Assets/Scripts/CharacterController/CharController.cs
@@ -10,18 +10,27 @@ public class CharController : MonoBehaviour{
 	public float maxSpeed = 5; //Max speed value allowed
 	public Animator anim; //Will be implemented once we have animations
 	
-	bool right = true;
+	bool right = true; //What Direction Is Player Facing
 
-	public Transform groundCheck;
-	public LayerMask groundType;
-	bool grounded = false;
-	float groundRadius = 0.2f;
+	public Transform groundCheck; //Transform to Check for Ground
+	public LayerMask groundType; //Define What Is Ground
+	bool grounded = false; //Is player on ground
+	float groundRadius = 0.2f; //Groundcheck radius
 
-	bool jump = false;
+	bool jump = false; //Is player jumping
 	public float jumpForce = 300f; //Max amount of jump force given
 
-	bool roll = false;
-	public float rollForce = 600f;
+	bool roll = false; //Is player rolling
+	public float rollForce = 5000f; //Roll force given
+
+	int punch = 0; //Is player punching. Int used to allow easy checking of punch states (combos, etc)
+	//0 = Idle/Not Punching
+	//1 = Default Punch
+	//2 = Second Punch
+	//3 = Third Punch / Finisher
+
+	//4 = Air Punch
+	//Unsure if we need more than that, unless we're allowing air punch combos which is usually kind of odd
 
 	void Start(){
 		anim = gameObject.GetComponent<Animator>(); //Will be implemented once we have animations
@@ -40,40 +49,89 @@ public class CharController : MonoBehaviour{
 			}else{
 				rigidbody2D.AddForce(new Vector2(-rollForce, 0f));
 			}
-			
 			roll = true;
 		}
 
+		if(Input.GetButtonDown("Fire2") && !roll){ //Punch
+
+			switch(punch){
+				case 0: //First time pressed
+					if(grounded){ //Ground Contextual Attack Types
+						punch = 1;
+						//Apply Attack Damage Here. Probably using a method for this.
+					}else{
+						punch = 4;
+						//Apply Attack Damage Here. Probably using a method for this.
+					}
+					break;
+
+				case 1: //Second time pressed
+					punch = 2;
+					//Apply Attack Damage Here. Probably using a method for this.
+					break;
+
+				case 2: //Third Time Pressed
+					punch = 3;
+					//Apply Attack Damage Here. Probably using a method for this.
+					break;
+			}
+			
+
+			
+		}
 			
 	}
 
 	void FixedUpdate(){
 		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundType); //Are we on ground
-		anim.SetBool("ground",grounded);
-		anim.SetFloat("vSpeed",rigidbody2D.velocity.y);
+		anim.SetBool("ground",grounded); //Let the Animator Know
+		anim.SetFloat("vSpeed",rigidbody2D.velocity.y); //How fast are we going vertically
 		
 		float move = Input.GetAxis ("Horizontal"); //Get horizontal input
 
-		anim.SetFloat("Speed",Mathf.Abs(move));
+		anim.SetFloat("xSpeed",Mathf.Abs(move)); //How fast are we going horizontally
 
 		rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y); //Take input and move object
 
+		//Orientation
 		if (move > 0 && !right){
 			Flip();
 		}else if (move < 0 && right){
 			Flip();
 		}
 
+		//Jump Animation
 		if(jump){
 			anim.SetTrigger("jump");
 
 			jump = false;
 		}
 
+		//Roll Animation
 		if(roll){
 			anim.SetTrigger("roll");
 
 			roll = false;
+		}
+
+		//Punch Animation
+		if(punch > 0){ //We have punch
+			switch(punch){
+				case 1:
+					//trigger for regular punch animation
+					break;
+				case 2:
+					//trigger for second punch animation
+					break;
+				case 3:
+					//trigger for third punch animation
+					punch = 0;
+					break;
+				case 4:
+					//trigger for air punch animation
+					punch = 0;
+					break;
+			}
 		}
 
 	}
@@ -87,4 +145,5 @@ public class CharController : MonoBehaviour{
 		scale.x = scale.x * -1;
 		transform.localScale = scale;
 	}
+
 }
