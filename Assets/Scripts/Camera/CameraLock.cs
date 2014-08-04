0785e2753618c23e997496ff0760fec871212441
MatthewWalker42 <matthew.j.walker42@gmail.com>
2014-08-03 20:08:52 -0400

    Camera is up and operational.
    Camera is up and operational. It follows the player and then when the
    player comes into contact with a camera lock object, it locks in
    position.

---
+++ Assets/Scripts/Camera/CameraLock.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLock : MonoBehaviour {
+
+	public GameObject mCamera;
+	//protected GameObject LockandRoll;
+
+	// Use this for initialization
+	void Start () {
+		//LockandRoll = mCamera.transform.GetComponent<CameraBehavior>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D crash)
+	{
+		mCamera.transform.GetComponent<CameraBehavior>().lockActive = true;
+		Destroy(this.gameObject);
+	}
+}
